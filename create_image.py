a09fda89e721bd191e6296532673a05fb12eece7
Neira Delgado -X (nedelgad - NUB78 SAS at Cisco) <nedelgad@cisco.com>
2022-02-10 16:15:39 -0500

    version-1.1.1 Mapping specific errors

--- create_image.py
+++ create_image.py
@@ -12,17 +12,26 @@ def generate_image(name, date, number):
     draw = ImageDraw.Draw(im)
 
     #set font
-    font_text = ImageFont.truetype('arial.ttf', 64)
-    font_text2 = ImageFont.truetype('arial.ttf', 80)
+    font_text = ImageFont.truetype('pixel.ttf', 80)
+    font_text2 = ImageFont.truetype('pixel.ttf', 80)
 
     #Draw Name, date and errors number
-    draw.text((65, 50), pres % name ,(68, 84, 106), font=font_text)
-    draw.text((60, 320), date ,(68, 84, 106), font=font_text)
-    draw.text((1000, 320), str(number) ,(68, 84, 106), font=font_text2)
+    draw.text((65, 105), pres % name ,(68, 84, 106), font=font_text)
+    draw.text((230, 630), date ,(237, 59, 59), font=font_text)
+    draw.text((1100, 630), str(number) ,(237, 59, 59), font=font_text)
 
-    resized_img = im.resize((711,400))
+    resized_img = im.resize((700,393))
     resized_img.save('imagedraw.png', width='400px')
 
     #im.show()
 
-generate_image('Name', 'date', 1)
+def generate_image_congrat():    
+    
+    #Import template
+    im = Image.open('template2.jpg')
+
+    resized_img = im.resize((700,496))
+    resized_img.save('imagedraw.png', width='400px')
+
+
+generate_image_congrat()
